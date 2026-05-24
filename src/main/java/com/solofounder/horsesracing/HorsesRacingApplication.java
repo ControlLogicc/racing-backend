c909b0721fe8b1a64c9ac0797a5fbdc21c26894b
Hao <hta022563@gmail.com>
2026-05-24 14:41:56 +0700

    origin

---
+++ src/main/java/com/solofounder/horsesracing/HorsesRacingApplication.java
@@ -0,0 +1,13 @@
+package com.solofounder.horsesracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorsesRacingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorsesRacingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solofounder/horsesracing/HorsesRacingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solofounder.horsesracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorsesRacingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
